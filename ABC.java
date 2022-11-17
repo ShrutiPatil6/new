97184d3e981bed3876ed562956bb3f1b03cdac22
ShrutiPatil6 <118419488+ShrutiPatil6@users.noreply.github.com>
2022-11-17 10:24:23 +0530

    Add files via upload

---
+++ ABC.java
@@ -0,0 +1,15 @@
+class Abc
+{
+    public static void main(String args[])
+    {
+    System.out.println("Hello World");
+    int a=10;
+    int b=20;
+    int c=5;
+    //char d=65;
+    //boolean bb=false;
+    System.out.println ("The result is "+ ((a*b)+c));
+    //System.out.println(d);
+    //System.out.println(bb);
+    }
+}
